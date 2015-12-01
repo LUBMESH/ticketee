6b572fb29f3f89dcc72264da408fdbecb046e738
Michiel Schukking <lmschukking@zoho.com>
2015-12-01 14:03:37 +0100

    Add feature for archiving users, including protection against self-archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  scope :excluding_archived, lambda { where(archived_at: nil)}
   # Override the to_s on the User model to provide a string containing 
   # the email and admin status of the user.
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
   end 
+  def archive
+    self.update(archived_at: Time.now)
+  end
 end
