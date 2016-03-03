f3d4d80323f9dbbdb0b323a1bdde3a7602a39218
Michiel Schukking <l.m.schukking@gmail.com>
2016-03-03 12:38:16 +0100

    Handle authorization errors gracefully

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+
   it "handles a missing project correctly" do
     get :show, id: "not-here"
 
@@ -9,4 +10,16 @@ RSpec.describe ProjectsController, type: :controller do
     message = "The project you were looking for could not be found."
     expect(flash[:alert]).to eq message
   end
+
+  it "handles permission errors by redirecting to a save place" do
+    allow(controller).to receive(:current_user)
+
+    project = FactoryGirl.create(:project)
+    get :show, id: project
+
+    expect(response).to redirect_to(root_path)
+    message = "You aren't allowed to do that."
+    expect(flash[:alert]).to eq message
+  end
+
 end
