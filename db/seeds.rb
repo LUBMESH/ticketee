ab5619d5f16f288de3371f47d2c179e00d4b4b0f
Michiel Schukking <l.m.schukking@gmail.com>
2016-06-01 13:24:16 +0200

    Prettify states.

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ end
 
 unless State.exists?
   State.create(name: "New", color: "#0066cc")
-  State.create(name: "Open", color: "008000")
-  State.create(name: "Closed", color: "990000")
-  State.create(name: "Awesome", color: "663399")
+  State.create(name: "Open", color: "#008000")
+  State.create(name: "Closed", color: "#990000")
+  State.create(name: "Awesome", color: "#663399")
 end
