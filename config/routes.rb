06e238b4c71ca96be0a479df18ca80400280171e
Michiel Schukking <l.m.schukking@gmail.com>
2016-06-02 11:27:26 +0200

    Admins can now set a default state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
-
     resources :projects, only: [:new, :create, :destroy]
-    resources :states, only: [:index, :new, :create]
+    resources :states, only: [:index, :new, :create] do
+      member do
+        get :make_default
+      end
+    end
     resources :users do
       member do
         patch :archive
@@ -13,15 +16,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'projects#index'
-
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
-
   resources :tickets, only: [] do
     resources :comments, only: [:create]
   end
-
   resources :attachments, only: [:show, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
