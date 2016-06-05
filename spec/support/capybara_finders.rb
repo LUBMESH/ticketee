0252e9dece4af5e04b4d5ebb88f0bfe9d1274904
Michiel Schukking <l.m.schukking@gmail.com>
2016-06-05 15:00:32 +0200

    Add the ability for users to remove tags from tickets

--- spec/support/capybara_finders.rb
+++ spec/support/capybara_finders.rb
@@ -3,6 +3,10 @@ module CapybaraFinders
     find("ul:not(.actions) li", text: content)
   end
 
+  def tag(content)
+    find("div.tag", text: content)
+  end
+
   RSpec.configure do |c|
     c.include CapybaraFinders, type: :feature
   end
