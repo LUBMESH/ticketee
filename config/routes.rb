ff75f45f293e441cf513c62bd1b9bc52a40d1a94
Michiel Schukking <l.m.schukking@gmail.com>
2016-06-08 12:14:00 +0200

    Add button so users can toggle watching on a ticket

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
       collection do
         get :search
       end
+
+      member do
+        post :watch
+      end
     end
   end
   resources :tickets, only: [] do
